e0111b705a2d8195dfb10bfb8f1a5ba12a262b45
JVN <johnvnettady@gmail.com>
2025-08-16 21:17:21 +0530

    fix: naaa that did not fix it , this finally fixes the font

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${plusJakartaSans.variable} ${inter.variable} antialiased`}>{children}</body>
+      <body className={`${plusJakartaSans.variable} ${inter.variable} font-primary antialiased`}>{children}</body>
     </html>
   );
 }
